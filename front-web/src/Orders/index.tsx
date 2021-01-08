7602e189262f96c941b3364cc2499b7dfb714bea
Frederico Balieiro <fredericoahb@gmail.com>
2021-01-08 12:29:44 -0300

    Search order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,31 +1,27 @@
 import { useEffect, useState } from 'react';
 import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './styles.css';
-import { Product } from './types';
+import { OrderLocationData, Product } from './types';
 
-function Orders(){
+const Orders = () => {
   const [products, setProducts] = useState<Product[]>([]);
-
-  console.log(products);
-
+  const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
   useEffect(() => {
-    
-    fetchProducts()
-      .then(response => setProducts(response.data))
-      .catch(error => console.log(error))
-
+      fetchProducts()
+          .then(response => setProducts(response.data))
+          .catch(error => console.log(error));
   }, [])
 
-
-  
-  return(
-    <div className="orders-container">
-      <StepsHeader />
-      <ProductsList products={products}/>
-    </div>
-  )
-}
+  return (
+      <div className="orders-container">
+          <StepsHeader />
+          <ProductsList products={products} />
+          <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
+      </div>
+  );
+};
 
 export default Orders;
--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const base_url = 'https://fred-deliver.herokuapp.com';
 
+const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
+
 export function fetchProducts(){
   return axios(`${base_url}/products`)
 }
+
+export const fetchLocalMapBox = (local: string) => 
+    axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`);
