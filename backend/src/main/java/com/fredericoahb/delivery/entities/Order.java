ea46ad4c91761887070f6a87837b8d8b9c643a5a
Frederico Balieiro <fredericoahb@gmail.com>
2021-01-11 10:38:24 -0300

    Fix total

--- backend/src/main/java/com/fredericoahb/delivery/entities/Order.java
+++ backend/src/main/java/com/fredericoahb/delivery/entities/Order.java
@@ -16,8 +16,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable{
-
+public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -28,13 +27,13 @@ public class Order implements Serializable{
 	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
-
+	
 	@ManyToMany
-	@JoinTable(name = "tb_order_product", 
+	@JoinTable(name = "tb_order_product",
 		joinColumns = @JoinColumn(name = "order_id"),
 		inverseJoinColumns = @JoinColumn(name = "product_id"))
 	private Set<Product> products = new HashSet<>();
-
+	
 	public Order() {
 	}
 
@@ -95,6 +94,14 @@ public class Order implements Serializable{
 	public void setStatus(OrderStatus status) {
 		this.status = status;
 	}
+	
+	public Double getTotal() {
+		double sum = 0;
+		for (Product p : products) {
+			sum += p.getPrice();
+		}
+		return sum;
+	}
 
 	public Set<Product> getProducts() {
 		return products;
@@ -124,5 +131,4 @@ public class Order implements Serializable{
 			return false;
 		return true;
 	}
-
 }
